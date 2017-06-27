e95c145643d219f73d54ab049c26bd154608210a
ajboard13 <aaronjboard@live.com>
2017-06-27 13:06:52 -0400

    added a list view to show the types of games

--- app/src/main/java/com/example/android/dominiontracker/ProfileActivity.java
+++ app/src/main/java/com/example/android/dominiontracker/ProfileActivity.java
@@ -4,7 +4,10 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -15,6 +18,7 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
     private FirebaseAuth firebaseAuth;
     private TextView textViewUserEmail;
     private Button buttonSignOut;
+    private ListView gameTypeList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +29,19 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
 
         textViewUserEmail = (TextView) findViewById(R.id.textViewUserEmail);
         buttonSignOut = (Button) findViewById(R.id.buttonSignOut);
+        gameTypeList = (ListView) findViewById(R.id.games_listview);
+
+
+        String[] gameTypes = getResources().getStringArray(R.array.game_types);
+        ArrayAdapter<String> gameTypeAdapter=new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, gameTypes);
+
+        gameTypeList.setAdapter(gameTypeAdapter);
+        gameTypeList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // TODO Auto-generated method stub
+            }
+        });
 
         if (firebaseAuth.getCurrentUser() == null){
             finish();
@@ -45,4 +62,6 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
             startActivity(new Intent(this,LoginActivity.class));
         }
     }
+
+
 }
